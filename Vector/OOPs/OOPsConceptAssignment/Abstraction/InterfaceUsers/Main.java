8210a4edd2bdd4f9d5e84a85258b30d5134a1bac
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-06-27 17:27:30 +0530

    Create Main.java

---
+++ Vector/OOPs/OOPsConceptAssignment/Abstraction/InterfaceUsers/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] arg) {
+        Playable guitar = new Guitar();
+        Playable piano = new Piano();
+
+        guitar.play();
+        piano.play();
+    }
+}
+// Playing the Guitar
+// Playing the Piano
