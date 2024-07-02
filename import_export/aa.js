7624b4463001ba922aa6762685752b9c64606036
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-07-02 18:10:28 +0530

    Add files via upload

---
+++ import_export/aa.js
@@ -0,0 +1,22 @@
+// module.exports.abc = ()=>{
+//     console.log("Hello abc")
+// }
+
+
+// module.exports.abcd = ()=>{
+//     console.log("Hello abcd")
+// }
+
+// module.exports.ab = ()=>{
+//     console.log("Hello ab")
+// }
+
+// const abcd = ()=>{
+//     console.log("Hello abcd")
+// }
+
+// const ab = ()=>{
+//     console.log("Hello ab")
+// }
+
+// module.exports = {abcd, ab};
---
+++ import_export/xyz.js
@@ -0,0 +1,7 @@
+const Abc = require("./abc");
+const {ab, abc, abcd} = require('./aa')
+let a = new Abc('Rani')
+a.printName()
+ab()
+abc()
+abcd()
